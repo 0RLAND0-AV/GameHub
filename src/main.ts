8a632671ecd29157bbeae3a78610dad4d42aeaf3
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-11-15 02:38:07 -0400

    feat: estructura inicial del proyecto

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import { ENV } from './config/env.config';
 import { initSocketIO } from './config/socketio.config';
 
 
-//funcion para reintentar conectar a BD
-// async function connectWithRetry() {
-//   try {
-//     await prisma.authenticate();
-//     console.info('Database connected');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//     setTimeout(connectWithRetry, 5000);
-//   }
-// }
+
 async function start() {
     try {
         // await connectWithRetry()
